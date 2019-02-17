29d861a56b9fc1bb8dfbf4e6850fa20e274a3bfa
yuki tanoue <llllllllllv0@gmail.com>
2019-02-17 15:54:09 +0900

    ナビゲーションにアイコンを追加

--- src/pages/Navigation.vue
+++ src/pages/Navigation.vue
@@ -1,21 +1,67 @@
 <template>
-  <b-nav
-    justified
-  >
-    <b-nav-item v-scroll-to="'#top-page'">
-      Top Page
-    </b-nav-item>
-    <b-nav-item v-scroll-to="'#profile'">
-      Profile
-    </b-nav-item>
-    <b-nav-item v-scroll-to="'#career'">
-      Career
-    </b-nav-item>
-    <b-nav-item v-scroll-to="'#skill'">
-      Skill
-    </b-nav-item>
-    <b-nav-item v-scroll-to="'#contact'">
-      Contact
+  <b-nav justified>
+    <b-nav-item
+      v-for="naviItem in navigationItems"
+      :key="naviItem.name"
+      v-scroll-to="naviItem.scrollId"
+    >
+      <div class="d-flex align-items-center">
+        <img
+          class="navigation-icon"
+          :src="naviItem.icon"
+        >
+        {{ naviItem.name }}
+      </div>
     </b-nav-item>
   </b-nav>
 </template>
+
+<script>
+import TopPageIcon from '../assets/toppage.png';
+import ProfileIcon from '../assets/profile.png';
+import SkillIcon from '../assets/skill.png';
+import CareerIcon from '../assets/career.png';
+import ContactIcon from '../assets/contact.png';
+
+export default {
+  data() {
+    return {
+      navigationItems: [
+        {
+          name: 'Top Page',
+          scrollId: '#top-page',
+          icon: TopPageIcon,
+        },
+        {
+          name: 'Profile',
+          scrollId: '#profile',
+          icon: ProfileIcon,
+        },
+        {
+          name: 'Career',
+          scrollId: '#career',
+          icon: CareerIcon,
+        },
+        {
+          name: 'Skill',
+          scrollId: '#skill',
+          icon: SkillIcon,
+        },
+        {
+          name: 'Contact',
+          scrollId: '#contact',
+          icon: ContactIcon,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+.navigation-icon {
+  width: 20px;
+  height: 20px;
+}
+</style>
--- src/pages/TopPage.vue
+++ src/pages/TopPage.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>TopPage</div>
+  <div class="top-page position-relative">
+    <div class="title">
+      <h1>
+        Tanoue Yuki
+      </h1>
+      <h2>
+        portfolio
+      </h2>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.top-page {
+  background-image: url('../assets/notebook.jpg');
+  background-size: cover;
+}
+.title {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50%;
+  margin: auto;
+}
+</style>
